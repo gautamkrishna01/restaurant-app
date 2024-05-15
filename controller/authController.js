03bac176690b9a0fa587339a2ccfebcd977141e1
Krishna Gautam <gtmkrishna400@gmail.com>
2024-05-15 07:38:43 +0545

    feat: jwt token implement

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+const JWT_SECRET = "mysecretvalue";
 
 // register controller methods
 const registerController = async (req, resp) => {
@@ -19,8 +21,8 @@ const registerController = async (req, resp) => {
     //hashing password
     const salt = await bcrypt.genSaltSync(10);
     const hashPassword = await bcrypt.hash(password, salt);
-    // creating new user
 
+    // creating new user
     const newUser = await userModel.create({
       username,
       email,
@@ -53,14 +55,18 @@ const loginController = async (req, resp) => {
     if (!isMatch) {
       resp.status(500).json({ message: "Invalid credentials" });
     }
+
+    //genetate the token
+
+    const token = JWT.sign({ id: user._id }, JWT_SECRET);
+
     if (!user) {
       return resp
         .status(404)
         .json({ message: "User not found or password mismatch" });
     }
-    // compare password
-    // If user found and password matches
-    resp.status(200).json({ message: "Login successful", user });
+
+    resp.status(200).json({ message: "Login successful", user, token });
   } catch (error) {
     resp.status(500).json({ message: "Error in login" });
   }
